8e1b5da79c256d2c2eeabef8ee3fd0fad9b37396
Robin Schneider <rschneider@vwd.com>
2021-02-12 13:01:26 +0100

    fix

--- gen.py
+++ gen.py
@@ -6,57 +6,70 @@ from io import BytesIO
 from sys import argv
 import pyAesCrypt
 import io
-
-if len(argv) != 4:
-    print("\n use old_main.py {KEY}\n int(password length) int(password amount) example: ./old_main.py savepw 4 2")
-    exit()
+import os
+from dotenv import load_dotenv
+import sqlalchemy as db
+import pandas as pd
 
 # ------------ conf ------------
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num = "0123456789"
-symbols = "[]{}()#*~.:,;<>-_€§$&?+^"
+symbols = "[]{}()#*~.:;<>-_€§$&?+^"
+main_pool = lower + upper + num + symbols
 
-pools = lower + upper + num + symbols
+main_buffer_size = 64 * 1024
 
-# key = argv[1]
-# length = int(argv[2])
-# amount =
+load_dotenv()
 
+file = open("cache.txt", "a")
+file.close()
 
-# passwords = []
-passwords_crypt = []
+if os.getenv("PG_DECRYPT_TYPE") == "db":
+    sql_type = os.getenv("PG_SQL_TYPE")
+    sql_user = os.getenv("PG_SQL_USER")
+    sql_passwd = os.getenv("PG_SQL_PASSWD")
+    sql_ip = os.getenv("PG_SQL_IP")
+    sql_table = os.getenv("PG_SQL_TABLE")
+
+    engine = db.create_engine(sql_type + '://' + sql_user + ':' + sql_passwd + '@' + sql_ip + '/' + sql_table)
+    connection = engine.connect()
+else:
+    connection = ""
+    pass
 
 
 # ------------ import and decrypt file ------------
-def get(key):
-    cache = open("cache.txt", "ab")
+def decrypt(key, buffer_size, connection):
     passwords_decrypt = []
-    bufferSize = 64 * 1024
-    get_cache = open("cache.txt", "rb")
-    crypt = get_cache.read()
-    crypt = crypt.split(b"AES")
-    for passwd in crypt:
+    print("reading cache")
+    if os.getenv("PG_DECRYPT_TYPE") == "db":  # get env PG_FILE_NAME (ste to db to decrypt the db)
+
+        df = pd.read_sql_table('passwd', connection)  # read table
+        get_cache = df.passwd
+    else:
+        get_cache = open("cache.txt", "rb")
+        get_cache = get_cache.read()
+        get_cache = get_cache.split(b"AES")
+    for passwd in get_cache:
         passwd = b"AES" + passwd
         try:
             fCiph: BytesIO = io.BytesIO(passwd)
             fDec = io.BytesIO()
             ctlen = len(passwd)
             fCiph.seek(0)
-            pyAesCrypt.decryptStream(fCiph, fDec, key, bufferSize, ctlen)
+            pyAesCrypt.decryptStream(fCiph, fDec, key, buffer_size, ctlen)
             passwords_decrypt.append(str(fDec.getvalue().decode("utf-8")))
         finally:
             continue
-    get_cache.close()
-    return passwords_decrypt
+    return passwords_decrypt, get_cache
 
 
 # ------------ gen ------------
-def gen(key, amount, length, pool):
-    passwords_decrypt = get(key)
+def gen(key, amount, length, pool, buffer_size):
+    passwords_decrypt = decrypt(key, buffer_size)
     passwords = []
-    while len(
-            passwords) < amount:  # as long as the existing passwords are less than the desired amount generate new one
+    while len(passwords) < amount:  # as long as the existing passwords are less than the desired amount generate new
         passwd = "".join(random.sample(pool, length))
         if passwd not in passwords and passwd not in passwords_decrypt:  # check if password is already created
             passwords.append(passwd)  # add password to array
@@ -67,22 +80,41 @@ def gen(key, amount, length, pool):
 
 
 # ------------ crypt ------------
-def crypt(key, amount, length, pool):
-    passwords = gen(key, amount, length, pool)
-    bufferSize = 64 * 1024
+def crypt(key, amount, length, pool, buffer_size):
+    passwords = gen(key, amount, length, pool, buffer_size)
     cache = open("cache.txt", "ab")
+    print("encrypting ...")
     for passwd in passwords:
         fCiph = io.BytesIO()
-        fDec = io.BytesIO()
         pbdata = passwd.encode("utf-8")
         fIn = io.BytesIO(pbdata)
-        pyAesCrypt.encryptStream(fIn, fCiph, key, bufferSize)
-        crypt = fCiph.getvalue()
-        cache.write(crypt)
+        pyAesCrypt.encryptStream(fIn, fCiph, key, buffer_size)
+        cryptpw = fCiph.getvalue()
+        cache.write(cryptpw)
     cache.close()
 
 
-# print(passwords_crypt)
-# print(passwords)
+def get(key, buffer_size, mode, connection):
+    passwords, get_cache = decrypt(key, buffer_size, connection)
+    pwid = 1
+    num = 0
+    if mode == "-q":
+        for password in passwords:
+            print(str(pwid) + ": " + password)
+            pwid = pwid + 1
+    elif mode == "-a":
+        for crypt, password in zip(get_cache, passwords):
+            print(crypt)
+            print("ID: " + str(num) + "  ^^^^------------------------------> " + password +
+                  " <----------------------------------\n")
+            num = num + 1
 
-crypt(argv[1], int(argv[3]), int(argv[2]), pools)
+
+if argv[1] == "gen":
+    crypt(argv[2], int(argv[4]), int(argv[3]), main_pool, main_buffer_size)
+elif argv[1] == "get":
+    get(argv[2], main_buffer_size, "-q", connection)
+else:
+    print("\n use old_main.py {KEY} int(password length) int(password amount) "
+          "\n example: ./old_main.py save_word 4 2 \n")
+    exit()
